9d80bf6bbc53a1eeb99add2e25028d0f1ce895c0
Your Name <you@example.com>
2022-09-19 22:24:55 -0500

    init

---
+++ Section 2 Big O/BigO_n.py
@@ -0,0 +1,5 @@
+def looping():
+    for i in range(0,10):
+        print(i,end=" ")
+
+looping();
---
+++ Section 2 Big O/BigO_n^2.py
@@ -0,0 +1,8 @@
+def print_items(i,j):
+    for i in range(0,i):
+        for p in range(0,j):
+            print("[",i,",",p,"]",end=" ")
+        
+        print()
+
+print_items(10,10);
